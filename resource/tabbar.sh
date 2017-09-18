5272c4268b466ae75d55c4554009508727ba7139
Coder4 <liheyuan.bj@gmail.com>
2017-09-18 18:51:04 +0800

    v0.1 tabbar

---
+++ resource/tabbar.sh
@@ -0,0 +1,10 @@
+tabbar_camera_2196f3.svg  tabbar_camera_e7e7e7.svg  tabbar_crown_2196f3.svg  tabbar_crown_e7e7e7.svg  tabbar_home_2196f3.svg  tabbar_home_e7e7e7.svg
+
+inkscape -z -e tabbar_camera.png -w 32 -h 32 ./tabbar_camera_e7e7e7.svg
+inkscape -z -e tabbar_camera_selected.png -w 32 -h 32 ./tabbar_camera_2196f3.svg
+
+inkscape -z -e tabbar_home.png -w 32 -h 32 ./tabbar_home_e7e7e7.svg
+inkscape -z -e tabbar_home_selected.png -w 32 -h 32 ./tabbar_home_2196f3.svg
+
+inkscape -z -e tabbar_crown.png -w 32 -h 32 ./tabbar_crown_e7e7e7.svg
+inkscape -z -e tabbar_crown_selected.png -w 32 -h 32 ./tabbar_crown_2196f3.svg
