d9648e3760ed7d993f3223c38ff89d638302d0ba
Coder4 <liheyuan.bj@gmail.com>
2017-09-22 17:49:39 +0800

    FIX: Bug

--- pages/tab1/scroll-view/scroll-view.js
+++ pages/tab1/scroll-view/scroll-view.js
@@ -1,66 +1,35 @@
-// pages/tab1/scroll-view/scroll-view.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    toView: 'green'
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  upper: function (e) {
+    console.log(e)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  lower: function (e) {
+    console.log(e)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  scroll: function (e) {
+    console.log(e)
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  scrollToTop: function (e) {
+    this.setAction({
+      scrollTop: 0
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
+  tap: function (e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1],
+          scrollTop: (i + 1) * 200
+        })
+        break
+      }
+    }
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  tapMove: function (e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
+    })
   }
 })
